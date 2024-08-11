599ab1fc0e3a0fb4bca5ec74fb459392da4e85ab
Vicente Mata <matavic25@gmail.com>
2024-08-11 19:27:50 -0400

    implement the complete layout

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import React from 'react';
+import HeaderBox from '../../components/HeaderBox';
+import TotalBalanceBox from '../../components/TotalBalanceBox';
+import RightSidebar from '../../components/RightSidebar';
 
 const Home = () => {
+  const loggedIn = {
+    firstName: 'Vicente',
+    lastName: 'JSM',
+    email: 'matavic25@gmail.com',
+  } as User;
   return (
-    <div>Home</div>
-  )
-}
+    <section className='home'>
+      <div className='home-content'>
+        <header className='home-header'>
+          <HeaderBox
+            type='greeting'
+            title='Welcome'
+            user={loggedIn?.firstName || 'Guest'}
+            subtext='Access & manage your account and transactions efficiently.'
+          />
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1250.35}
+          />
+        </header>
+        RECENT TRANSACTIONS
+      </div>
+      <RightSidebar
+        user={loggedIn}
+        transactions={[{}, {}]}
+        banks={[{ currentBalance: 123.5 }, {}]}
+      />
+    </section>
+  );
+};
 
-export default Home
+export default Home;
--- lib/utils.ts
+++ lib/utils.ts
@@ -66,7 +66,7 @@ export const formatDateTime = (dateString: Date) => {
 };
 
 export function formatAmount(amount: number): string {
-  const formatter = new Intl.NumberFormat("en-US", {
+  const formatter = new Intl.NumberFormat("en-CO", {
     style: "currency",
     currency: "USD",
     minimumFractionDigits: 2,
